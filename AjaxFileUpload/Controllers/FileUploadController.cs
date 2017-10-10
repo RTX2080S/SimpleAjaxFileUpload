cc020608ef25d874fb60346d314f24d0b0785b9e
LL <52593730@qq.com>
2017-10-11 06:24:08 +0800

    Add the project

---
+++ AjaxFileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AjaxFileUpload.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        [HttpPost]
+        public void UploadFile()
+        {
+            if (HttpContext.Current.Request.Files.AllKeys.Any())
+            {
+                // Get the uploaded image from the Files collection
+                var httpPostedFile = HttpContext.Current.Request.Files["UploadedImage"];
+
+                if (httpPostedFile != null)
+                {
+                    // Validate the uploaded image(optional)
+
+                    // Get the complete file path
+                    var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/App_Data"), httpPostedFile.FileName);
+
+                    // Save the uploaded file to "UploadedFiles" folder
+                    httpPostedFile.SaveAs(fileSavePath);
+                }
+            }
+        }
+    }
+}
